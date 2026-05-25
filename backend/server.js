1720198d253fc55c9877f413054dc036c9ba4981
Harsh Vishwakarma <harsh17v@gmail.com>
2026-05-25 12:06:38 +0530

    Small change

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const app = require('./src/app')
 const connectDB = require('./src/config/database')
-
+const port = 3000
 
 connectDB()
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server is running on port 3000")
 })
